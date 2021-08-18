710c2de9172ae7b5d7a465d98fa3c9267e1ea289
surpawar <85701604+surpawar@users.noreply.github.com>
2021-08-18 11:19:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
